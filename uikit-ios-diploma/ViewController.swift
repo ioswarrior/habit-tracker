9d773a5a3f55bfc12a3365c831da258ce6b45989
iOS <ioswarrior@gmail.com>
2022-04-06 17:19:08 +0600

    Initial Commit

---
+++ uikit-ios-diploma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit-ios-diploma
+//
+//  Created by Muller Alexander on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
